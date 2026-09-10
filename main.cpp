6450d5237db88fb8e214cfcffa608fa1f5bd7fe0
celinem13 <celinemangahas13@gmail.com>
2026-09-09 21:54:02 -0700

    refactor: extract warning display function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,31 @@ struct TelemetryReading {
     double fuelPercent;
 };
 
+void displayWarnings(
+    const TelemetryReading& reading,
+    double highEngineTemperatureCelsius,
+    double lowFuelPercent
+) {
+    std::cout << "\nWarnings:\n";
+
+    bool hasWarning = false;
+
+    if (reading.engineTemperatureCelsius >=
+        highEngineTemperatureCelsius) {
+        std::cout << "- Engine temperature is too high!\n";
+        hasWarning = true;
+    }
+
+    if (reading.fuelPercent <= lowFuelPercent) {
+        std::cout << "- Fuel level is too low!\n";
+        hasWarning = true;
+    }
+
+    if (!hasWarning) {
+        std::cout << "- None\n";
+    }
+}
+
 int main() {
     const TelemetryReading reading{
         430.0,
@@ -33,24 +58,11 @@ int main() {
     std::cout << "Fuel: "
               << reading.fuelPercent << "%\n";
 
-    std::cout << "\nWarnings:\n";
-
-    bool hasWarning = false;
-
-    if (reading.engineTemperatureCelsius >=
-        highEngineTemperatureCelsius) {
-        std::cout << "- Engine temperature is too high!\n";
-        hasWarning = true;
-    }
-
-    if (reading.fuelPercent <= lowFuelPercent) {
-        std::cout << "- Fuel level is too low!\n";
-        hasWarning = true;
-    }
-
-    if (!hasWarning) {
-        std::cout << "- None\n";
-    }
+    displayWarnings(
+        reading,
+        highEngineTemperatureCelsius,
+        lowFuelPercent
+    );
 
     return 0;
 }
